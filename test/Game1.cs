18e71ae4b749cb88ee9ed25a059462a059b9d5e5
Jasper-Hens <hensjasper@gmail.com>
2025-12-04 16:03:44 +0100

    block klasse aangemaakt tilessheet werkt in game klasse.

--- test/Game1.cs
+++ test/Game1.cs
@@ -2,6 +2,8 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
+using System.Linq;
+using test.Blocks;
 using static System.Formats.Asn1.AsnWriter;
 
 namespace test
@@ -19,7 +21,12 @@ namespace test
 
         private Hero _hero;
 
-        private List<Rectangle> platforms;
+        private Texture2D _tilesSpriteSheet;
+        private Texture2D _brightBackGroundSpriteSheet;
+        private Texture2D _paleBackGroundSpriteSheet;
+        private Texture2D _ItemSpriteSheet;
+        private Texture2D _ObjectSpriteSheet;
+        private List<Block> _blocks;
 
         public Game1()
         {
@@ -29,9 +36,9 @@ namespace test
 
         protected override void Initialize()
         {
-            
+
             base.Initialize();
-            
+
 
         }
 
@@ -48,12 +55,30 @@ namespace test
 
             _hero = new Hero(_idleTexture, _runTexture, _jumpTexture);
 
-            // Platforms
-            platforms = new List<Rectangle>
-            {
-                new Rectangle(0, 400, 800, 50), // grond
-                new Rectangle(200, 310, 150, 20), // platform midden
-            };
+            _tilesSpriteSheet = Content.Load<Texture2D>("Tiles/tilesSpriteSheet");
+            _brightBackGroundSpriteSheet = Content.Load<Texture2D>("Background/BrightBackgroundSpriteSheet");
+            _paleBackGroundSpriteSheet = Content.Load<Texture2D>("Background/paleBackgroundSpriteSheet");
+            _ItemSpriteSheet = Content.Load<Texture2D>("Items/ItemSpriteSheet");
+            _ObjectSpriteSheet = Content.Load<Texture2D>("Objects/ObjectSpriteSheet");
+
+            _hero = new Hero(_idleTexture, _runTexture, _jumpTexture);
+
+            // INITIALISEER de blokken lijst
+            _blocks = new List<Block>();
+
+            // Definitie van de tile uitsnede (SourceRectangle): sprite12, 325, 79, 64, 50
+            Rectangle sourceTile = new Rectangle(325, 79, 64, 50);
+
+            // Definieer de World Positie/Hitbox (Destination Rectangle)
+            // Tip: Maak de grootte van de BoundingBox gelijk aan de SourceRectangle hoogte/breedte 
+            // zodat de tile correct wordt getekend zonder te stretchen.
+            Rectangle groundRect = new Rectangle(0, 400, 800, 50); // grond (stretchen is OK hier)
+            Rectangle platformRect = new Rectangle(200, 310, 128, 50); // platform midden
+
+            // MAAK de Block objecten aan en voeg ze toe
+            // Hier maak je voor het 'grond'-blok een NIEUW Block aan met de tile uitsnede
+            _blocks.Add(new Block(groundRect, sourceTile, _tilesSpriteSheet));
+            _blocks.Add(new Block(platformRect, sourceTile, _tilesSpriteSheet));
         }
 
         protected override void Update(GameTime gameTime)
@@ -64,7 +89,11 @@ namespace test
             _hero.IsRunningLeft = k.IsKeyDown(Keys.Left);
             _hero.Jump = k.IsKeyDown(Keys.Space);
 
-            _hero.Update(gameTime, platforms);
+            List<Rectangle> collisionRects = _blocks
+                 .Select(b => b.BoundingBox)
+                 .ToList();
+
+            _hero.Update(gameTime, collisionRects);
 
             base.Update(gameTime);
         }
@@ -78,9 +107,9 @@ namespace test
             // Hero
             _hero.Draw(_spriteBatch);
 
-            // Platforms
-            foreach (var plat in platforms)
-                _spriteBatch.Draw(_blokTexture, plat, Color.Green);
+            // Teken alle Block objecten
+            foreach (var block in _blocks)
+                block.Draw(_spriteBatch);
 
             // Hero hitbox debug
             _spriteBatch.Draw(_blokTexture, _hero.Hitbox.HitboxRect, Color.Red * 0.4f);
--- test/Level/Block.cs
+++ test/Level/Block.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
-namespace test.Level
+namespace test.Blocks
 {
-    internal class Block
+    public class Block
     {
+        // De rechthoek in de wereld. Dit is de HITBOX en de positie.
+        public Rectangle BoundingBox { get; private set; }
+
+        // De rechthoek op de spritesheet. Dit is de UITSNEDE.
+        public Rectangle SourceRectangle { get; private set; }
+
+        // De volledige tilesheet texture
+        private Texture2D _texture;
+
+        // Het Block is standaard niet passeerbaar (solide).
+        public bool IsPassable { get; private set; } = false;
+
+        /// <summary>
+        /// Constructor voor een level-blok.
+        /// </summary>
+        /// <param name="worldRect">De positie en grootte in de game wereld (de hitbox).</param>
+        /// <param name="sourceRect">De uitsnede op de tilesheet.</param>
+        /// <param name="texture">De tilesheet Texture2D.</param>
+        public Block(Rectangle worldRect, Rectangle sourceRect, Texture2D texture)
+        {
+            this.BoundingBox = worldRect;
+            this.SourceRectangle = sourceRect;
+            this._texture = texture;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Gebruik de overload die zowel de Source (uitsnede) als de Destination (wereld-positie) gebruikt.
+            // Dit tekent de tile (SourceRectangle) op de plaats van de hitbox (BoundingBox).
+            spriteBatch.Draw(_texture, BoundingBox, SourceRectangle, Color.White);
+        }
+
+        // Update methode voor eventuele animatie of beweging
+        public virtual void Update(GameTime gameTime)
+        {
+            // Voor statische blokken blijft deze leeg
+        }
     }
 }
